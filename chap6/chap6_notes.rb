458bbe0c23cf032cc186e9c58cf8572462af67b3
Nigel Said <nigelsaid@gmail.com>
2017-03-19 18:20:36 +0000

    finished chap6/chap6_notes.rb

--- chap6/chap6_notes.rb
+++ chap6/chap6_notes.rb
@@ -47,3 +47,50 @@ puts(str.center(line_width))
 puts(str.rjust(line_width))
 puts(str.ljust(line_width/2) + str.rjust(line_width/2))
 puts ''
+
+#6.4
+puts 5**2
+puts 5**0.5
+puts 7/3
+puts 7%3
+puts 365%7
+puts ''
+puts (5-2).abs
+puts (2-5).abs
+puts ''
+
+#6.5
+puts rand
+puts rand
+puts rand
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(1))
+puts(rand(1))
+puts(rand(1))
+puts(rand(9999999999999999999999999999999999999))
+puts('The weatherman said there is a')
+puts(rand(101).to_s + '% chance of rain,')
+puts('but you can never trust a weatherman.')
+puts ''
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ''
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ''
+
+#6.6
+puts(Math::PI)
+puts(Math::E)
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/4))
+puts(Math.log(Math::E**2))
+puts((1 + Math.sqrt(5))/2)
