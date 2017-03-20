8bd5c92e27e6e962aaac5eb68db753d0588cb07a
Nigel Said <nigelsaid@gmail.com>
2017-03-20 14:37:39 +0000

    added chap8\chap8_exercises.rb

---
+++ chap8/chap8_exercises.rb
@@ -0,0 +1,61 @@
+#8.0
+names = ['Ada', 'Belle', 'Chris']
+puts names
+puts
+puts names[0]
+puts names[1]
+puts names[2]
+puts names[3]
+puts
+
+other_peeps = []
+other_peeps[3] = 'beebee Meaner'
+other_peeps[0] = 'Ah-ha'
+other_peeps[1] = 'Seedee'
+other_peeps[0] = 'beebee Ah-ha'
+puts other_peeps
+puts
+
+#8.1
+languages = ['English', 'Norwegian', 'Ruby']
+languages.each do |lang|
+  puts 'I love ' + lang + '!'
+  puts 'Don\'t you?'
+end
+puts 'And let\'s hear it for Java!'
+puts '<crickets chirp in the distance>'
+puts
+
+3.times do
+  puts 'Hip-Hip-Hooray!'
+end
+puts
+2.times do
+  puts '...you can say that again...'
+end
+puts
+
+#8.2
+foods = ['artichoke', 'brioche', 'caramel']
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts
+puts foods.join(' :) ') + ' 8)'
+20.times do
+  puts []
+end
+puts
+
+favorites = []
+favorites.push 'raindrops on roses'
+favorites.push 'whiskey on kittens'
+puts favorites[0]
+puts favorites.last
+puts favorites.length
+puts favorites.pop
+puts favorites
+puts favorites.length
+puts
