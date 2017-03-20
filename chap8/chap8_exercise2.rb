38854f0e95e37cb3789115338325c4413347c49c
Nigel Said <nigelsaid@gmail.com>
2017-03-20 15:15:20 +0000

    added chap8_exercise2.rb

---
+++ chap8/chap8_exercise2.rb
@@ -0,0 +1,4 @@
+line_width = 30
+table_of_contents = [ ('Table of Contents'.center(line_width)), ('Chapter 1: Getting Started'.ljust(line_width) + 'page 1'.rjust(line_width)), ('Chapter 2: Numbers'.ljust(line_width) + 'page 9'.rjust(line_width)), ('Chapter 3: Letters'.ljust(line_width) + 'page 13'.rjust(line_width))  ]
+
+puts table_of_contents
